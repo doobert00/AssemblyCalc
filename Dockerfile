7ef9be0f8cc6374d94e4fee1df10b37abc3be2ea
doobert <dubinskyz00@gmail.com>
2023-12-30 14:47:51 -0500

    integer printing

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,13 @@ RUN apt-get update
 
 RUN apt-get install -y nasm binutils
 
-COPY hello.asm /app/
+COPY intprint.asm /app/
 
-RUN nasm -f elf -o /app/hello.o /app/hello.asm
-RUN ld -m elf_i386 -s -o /app/hello /app/hello.o
+RUN nasm -f elf -o /app/intprint.o /app/intprint.asm
+RUN ld -m elf_i386 -s -o /app/intprint /app/intprint.o
 
 FROM alpine:latest
 
-COPY --from=build /app/hello /app/
+COPY --from=build /app/intprint /app/
 
-CMD ["/app/hello"]
+CMD ["/app/intprint"]
---
+++ intprint.asm
@@ -0,0 +1,36 @@
+section .data
+	num1 db 1 ;
+	num2 db 2 ;
+
+section .text
+global _start
+_start:
+	mov eax, num1 ;
+	mov ebx, 0 ;
+	add eax, ebx  ; The numbers are summed in the eax register
+
+	; Convert EAX to ASCII and store it onto the stack
+	sub esp, 16 		; reserve space on stack
+	mov ecx, 1		; Number length
+	mov ebx, 16
+	.L1:
+	xor edx, edx 		; No idea what this does
+	div ecx      		; Extract the last decimal digit (why?)
+	or dl, 0x30  		; Convert everything else to ASCII (how?)
+	sub ebx, 1   		;
+	mov [esp+ebx], dl 	; Store the remainder on the stack in reverse...
+	test eax, eax 		; until there is nothing left to divide
+	jnz .L1
+
+	mov eax, 4		; SYS_WRITE
+	lea ecx, [esp+ebx]	; Point to the first ASCII digit
+	mov edx, 16		;
+	sub edx, ebx		; Count of digits
+	mov ebx, 1		; STDOUT
+	int 0x80		; syscall
+
+	add esp, 16		; restore the stack
+
+	mov eax, 1		; SYS_EXIT
+	xor ebx, ebx		; Return value
+	int 0x80		; syscall
